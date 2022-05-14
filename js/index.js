b5c0d14b568e1c60be1ce51c3ab1df6c0f9971ab
blazejjestem <blazej0wasielewski@gmail.com>
2022-05-14 12:41:58 +0200

    Hero section complete and responsive.

--- js/index.js
+++ js/index.js
@@ -7,3 +7,13 @@ navButton.addEventListener("click", () => {
   navButton.classList.toggle("nav__toggle-button--active");
   navMenu.classList.toggle("nav__menu--active");
 });
+// Navbar close on window resize > 1250 px
+window.onresize = () => {
+  if (
+    window.innerWidth >= 1250 &&
+    navMenu.classList.contains("nav__menu--active")
+  ) {
+    navMenu.classList.remove("nav__menu--active");
+    navButton.classList.remove("nav__toggle-button--active");
+  }
+};
